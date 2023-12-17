93de73fb3b1dec3ace2f2396b73a9c9d05337c95
sylotana <anatolyshangin@mail.ru>
2023-12-17 22:23:49 +0300

    Вынес локальную константу в глобальную область, изменил функцию рендера и удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-function renderCard(imageLink, name) {
+const PLACES_LIST = document.querySelector(".places__list");
+
+function renderCard(imageLink, name, removeFunc) {
   const cardTemplate = document.querySelector("#card-template").content;
 
   const card = cardTemplate.querySelector(".card").cloneNode(true);
@@ -7,25 +9,23 @@ function renderCard(imageLink, name) {
   const cardRemoveButton = card.querySelector(".card__delete-button");
 
   cardImage.src = imageLink;
+  cardImage.alt = name;
   cardTitle.textContent = name;
 
-  cardRemoveButton.addEventListener("click", removeCard);
+  cardRemoveButton.addEventListener("click", removeFunc);
 
   return card;
 }
 
-function removeCard() {
-  const card = this.parentElement;
+function removeCard(event) {
+  const card = event.target.closest('.card');
   card.remove();
 }
 
 function addCard(place) {
-  const placeList = document.querySelector(".places__list");
-  const card = renderCard(place.link, place.name);
+  const card = renderCard(place.link, place.name, removeCard);
 
-  placeList.append(card);
+  PLACES_LIST.append(card);
 }
 
-initialCards.forEach((element) => {
-  addCard(element);
-});
+initialCards.forEach(addCard);
